bb2bb2815c34471e13f9fc4c034cfcc07087e860
Marcin <grzechnikmarcin@wp.pl>
2025-09-03 18:03:15 +0200

    Aded new functions and fuzed all migrations to one.

--- crypto_app/models.py
+++ crypto_app/models.py
@@ -1,17 +1,32 @@
+import statistics
+from enum import StrEnum
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class Currency(StrEnum):
+    USD = "USD"
+    PLN = "PLN"
+    EUR = "EUR"
+    GBP = "GBP"
+
+
 CURRENCY_CHOICES = [
-        ("USD", "USD"),
-        ("PLN", "PLN"),
-        ("EUR", "Euro"),
-        ("GBP", "British Pound")
+    (Currency.USD, "US Dollar"),
+    (Currency.PLN, "Polish Zloty"),
+    (Currency.EUR, "Euro"),
+    (Currency.GBP, "British Pound"),
 ]
 
+
+class TransactionType(StrEnum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+
 TRANSACTION_TYPE_CHOICES = [
-    ("BUY", "Purchase"),
-    ("SELL", "Sale")
+    (TransactionType.BUY, "Purchase"),
+    (TransactionType.SELL, "Sale")
 ]
 
 
@@ -30,12 +45,35 @@ class Cryptocurrency(models.Model):
     def __str__(self):
         return f"{self.name} ({self.symbol})"
 
+    @property
+    def trading_recommendation(self):
+        latest_data = MarketData.objects.filter(cryptocurrency=self).order_by("-created_at").first()
+        if not latest_data:
+            return "Out of data"
+
+        last_price = latest_data.price
+
+        last_7_days = MarketData.objects.filter(cryptocurrency=self).order_by("-created_at")[:7]
+        if len(last_7_days) < 7:
+            return "Out of data"
+
+        average_price = statistics.mean([data.price for data in last_7_days])
+
+        percent_change = (last_price - average_price) / average_price * 100
+
+        if percent_change < -5:
+            return "Buy"
+        elif percent_change > 5:
+            return "Sell"
+        else:
+            return "Hold"
+
 
 class MarketData(models.Model):
     cryptocurrency = models.ForeignKey(Cryptocurrency, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=20, decimal_places=8)
     volume = models.DecimalField(max_digits=20, decimal_places=2)
-    date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default="PLN")
 
     def __str__(self):
@@ -43,12 +81,12 @@ class MarketData(models.Model):
 
 
 class Transaction(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
     cryptocurrency = models.ForeignKey(Cryptocurrency, on_delete=models.CASCADE)
     type = models.CharField(max_length=10, choices=TRANSACTION_TYPE_CHOICES)
     amount = models.DecimalField(max_digits=20, decimal_places=8)
     price = models.DecimalField(max_digits=20, decimal_places=2)
-    date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.type} - {self.amount} - {self.cryptocurrency.symbol}"
@@ -59,7 +97,7 @@ class Transaction(models.Model):
 
 
 class Portfolio(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
     cryptocurrency = models.ForeignKey(Cryptocurrency, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=20, decimal_places=8)
 
--- crypto_app/urls.py
+++ crypto_app/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
+    path("dashboard/", views.dashboard, name="dashboard"),
     path("cryptocurrencies/", views.cryptocurrencies, name="cryptocurrencies"),
     path("transactions/", views.transactions, name="transactions"),
     path("portfolio/", views.portfolio, name="portfolio"),
+    path("reports/", views.reports, name="reports"),
 
 ]
